d18515ef74a2762a4a025a733bfb20a9aefc04bc
Laura Felce <crepu83@yahoo.it>
2018-09-14 16:37:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,9 +16,8 @@ function whileLoop(n) {
   }
   
   function doWhileLoop(array) {
-    do { array.length = 0;
-    incrementVariable();}
-  while (array.length > 0 && incrementVariable);
+    do { array.length === 0;}
+  while (array.length > 0 && incrementVariable());
 return array;
     }
   
